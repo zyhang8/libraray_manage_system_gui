c93ea299cb03fb6af517ae4a5c297bfd91345e3d
zyhang8 <1023708557@qq.com>
2019-03-04 18:22:07 +0800

    update

--- 用户图形界面/Jython、Swing.py
+++
@@ -1,9 +0,0 @@
-from javax.swing import *
-import sys
-
-def hello(event): print 'Hello, world! '
-btn = JButton('Hello')
-btn.actionPerformed = hello
-
-win = JFrame('Hello, Swing!')
-win.contentPane.add(btn)
